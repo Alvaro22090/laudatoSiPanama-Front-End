309726a87fec0fd673dd846184ff67f91aced0f6
Alvaro <ahernandez220900@gmail.com>
2026-03-08 21:22:35 -0500

    Actualizando ambiente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'http://157.137.208.233',
-  wsUrl:  'http://157.137.208.233/ws',
+  apiUrl: 'https://laudatosi.duckdns.org',
+  wsUrl:  'https://laudatosi.duckdns.org/ws',
 };
